713b4db533dec61b6ae184ad530c907b32a55468
codebyimaad <codewithimaad@gmail.com>
2025-07-15 08:45:24 +0300

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,21 +1,11 @@
 <?php
-// setting time zone
 date_default_timezone_set("Africa/Kampala");
 
-// define the site root
 define('SITE_ROOT', 'http://130.162.171.209/');
 
-
-
 // Database Information
-// Database Hostname
-define('DATABASE_HOST','c4binventory_db')
-// Database Username
+define('DATABASE_HOST','c4binventory_db'); // container name
 define('DATABASE_USER','admin');
-// Database Name
-define('DATABASE_NAME', 'ample');
-// Database DB_PASS
+define('DATABASE_NAME','ample');
 define('DATABASE_PASS','root');
-
-
- ?>
+?>
